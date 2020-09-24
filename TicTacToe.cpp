9533a62bd8c1655580fdd4c31c4338bebdb059fb
Justin <juda8901@colorado.edu>
2020-09-23 19:53:25 -0600

    Revert "added DisplayBoard to display the tictactoe board in the terminal"
    This reverts commit 5601ff2eb3dc7cd59726c88709dcbc742dea02e1.

--- TicTacToe.cpp
+++ TicTacToe.cpp
@@ -13,29 +13,8 @@ char** CreateBoard()
     return board;
 }
 
-/**
-    Displays the tic tac toe board in the terminal
-
-    @param b    2D array representing the board to display
-*/
-void DisplayBoard(char **b){
-    for(int i=0; i<3; i++){
-        for(int j=0; j<3; j++){
-            std::cout << "\t" << b[i][j];
-            if(j==0 || j==1){
-                std::cout << "\t|";
-            }
-        }
-        if(i!=2){
-            std::cout << "\n\t----------------------------------";
-        }
-        std::cout << std::endl;
-    }
-}
-
 int main()
 {
     char** test = CreateBoard();
-    DisplayBoard(test);
     return 0;
 }
